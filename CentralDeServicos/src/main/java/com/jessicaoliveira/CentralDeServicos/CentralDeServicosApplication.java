8ae009b20a305e7812b1c25890a366b46629d131
Jéssica Caroline <jessicacaroline3213@hotmail.com>
2024-06-27 05:55:01 -0300

    created project

---
+++ CentralDeServicos/src/main/java/com/jessicaoliveira/CentralDeServicos/CentralDeServicosApplication.java
@@ -0,0 +1,13 @@
+package com.jessicaoliveira.CentralDeServicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralDeServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralDeServicosApplication.class, args);
+	}
+
+}
---
+++ CentralDeServicos/src/test/java/com/jessicaoliveira/CentralDeServicos/CentralDeServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessicaoliveira.CentralDeServicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDeServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
