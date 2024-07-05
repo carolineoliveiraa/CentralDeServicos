27593e93e9ee064b3bf213d24ec5fb714a5e8a1b
Jéssica Caroline <jessicacaroline3213@hotmail.com>
2024-07-04 23:32:35 -0300

    updating work orders

--- CentralDeServicos/src/main/java/com/jessicaoliveira/CentralDeServicos/resources/OsResource.java
+++ CentralDeServicos/src/main/java/com/jessicaoliveira/CentralDeServicos/resources/OsResource.java
@@ -4,10 +4,10 @@ import com.jessicaoliveira.CentralDeServicos.dtos.OSDTO;
 import com.jessicaoliveira.CentralDeServicos.services.OsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/os")
@@ -20,5 +20,17 @@ public class OsResource {
     public ResponseEntity<OSDTO> findById(@PathVariable Integer id){
         OSDTO obj = new OSDTO(service.findById(id));
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OSDTO>> findAll(){
+        List<OSDTO> list = service.findAll().stream().map
+                (obj -> new OSDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<OSDTO> create(@RequestBody OSDTO obj){
+
     }
 }
